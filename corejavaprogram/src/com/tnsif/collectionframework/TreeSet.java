395595bc1393331943668c828c73eb812ce20cf2
CSELAB6 <CSELAB6@DESKTOP-T1EPKUT>
2025-02-19 12:00:30 +0530

    set

---
+++ corejavaprogram/src/com/tnsif/collectionframework/TreeSet.java
@@ -0,0 +1,25 @@
+package com.tnsif.collectionframework;
+
+import java.util.TreeSet;
+
+public class TreeSet {
+	public static void tressetoperation() {
+		TreeSet<Integer>t=new TreeSet<Intger>();
+		
+		t.add(78);
+		t.add(8);
+		t.add(58);
+		t.add(3);
+		t.add(3);
+		t.add(null);
+		
+		System.out.println("elements of treeset"+t);
+	}
+	public static void main(String[] args) {
+		TreeSet.tressetoperation();
+	}
+		
+		
+	}
+
+
---
+++ corejavaprogram/src/com/tnsif/setdemo/Setoperations.java
@@ -0,0 +1,32 @@
+package com.tnsif.setdemo;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Setoperations {
+	
+public static void operations() {
+	
+
+	Set<Integer>num=new HashSet<Integer>();
+	num.add(12);
+	num.add(13);
+	
+	num.addAll(Arrays.asList(new Integer[]{1,2,3,4}));
+	System.out.println(num);
+	
+	Set<Integer>Oddset=new HashSet<Integer>();
+	Oddset.addAll(Arrays.asList(new Integer[] {5,6,7,2}));
+	
+	Set<Integer>intersection=new HashSet<Integer>(num);
+	intersection.retainAll(Oddset);
+	System.out.println("intersection"+intersection);
+	
+	Set<Integer>union=new HashSet<Integer>(num);
+	union.addAll(Oddset);
+	System.out.println("union"+union);
+	
+	}
+}
+
