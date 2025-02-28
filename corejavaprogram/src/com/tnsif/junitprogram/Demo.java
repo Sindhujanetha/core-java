82595f0cfacb5877d244a053edef163ccf58cc6e
CSELAB6 <CSELAB6@DESKTOP-T1EPKUT>
2025-02-28 11:26:20 +0530

    module

---
+++ corejavaprogram/src/com/tnsif/junitprogram/Demo.java
@@ -0,0 +1,9 @@
+package com.tnsif.junitprogram;
+
+public class Demo {
+	
+	public int add(int a,int b) {
+		return (a+b);
+	}
+
+}
--- corejavaprogram/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module corejavaprogram {
-	requires java.sql;
-}
